4bf8ef7d76e64e839e0173302c2280e885d0037d
shinhasegawa1223 <shin.hasegawa1223@gmail.com>
2022-09-20 21:30:41 +0900

    js comment

--- src/main/resources/static/js/user_info.js
+++ src/main/resources/static/js/user_info.js
@@ -1,7 +1,12 @@
+/*
+モーダル画面にIDを受け渡すロジック
+*/
 
-   let delinfo = function (myvalue) {
-   const modalresult = document.getElementById('deluser');
-    console.log(myvalue);
-		console.log(modalresult);
-		modalresult.setAttribute('value', myvalue);
-    }
+let delinfo = function(myvalue) {
+	const modalresult = document.getElementById('deluser');
+	/*
+	 console.log(myvalue);
+	 console.log(modalresult);
+	 */
+	modalresult.setAttribute('value', myvalue);
+}
